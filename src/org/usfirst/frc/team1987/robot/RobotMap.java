926a65039c7b23954c537299f447e5dae5a84962
LivLittle <olittle0424@gmail.com>
2018-02-03 13:28:05 -0600

    Config drive TalonSRXs and encoders.

--- src/org/usfirst/frc/team1987/robot/RobotMap.java
+++ src/org/usfirst/frc/team1987/robot/RobotMap.java
@@ -14,13 +14,11 @@ package org.usfirst.frc.team1987.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
-
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
+	public static int leftMasterID = 1;
+	public static int leftSlave1ID = 2;
+	public static int leftSlave2ID = 3;
+	public static int rightMasterID = 4;
+	public static int rightSlave1ID = 5;
+	public static int rightSlave2ID = 6;
+	
 }
--- src/org/usfirst/frc/team1987/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team1987/robot/subsystems/Drive.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team1987.robot.subsystems;
 
+import org.usfirst.frc.team1987.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,8 +13,31 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Drive extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	private final WPI_TalonSRX leftMaster;
+	private final WPI_TalonSRX leftSlave1;
+	private final WPI_TalonSRX leftSlave2;
+	private final WPI_TalonSRX rightMaster;
+	private final WPI_TalonSRX rightSlave1;
+	private final WPI_TalonSRX rightSlave2;
+	
+	public Drive() {
+		leftMaster = new WPI_TalonSRX(RobotMap.leftMasterID);
+		leftSlave1 = new WPI_TalonSRX(RobotMap.leftSlave1ID);
+		leftSlave2 = new WPI_TalonSRX(RobotMap.leftSlave2ID);
+		rightMaster = new WPI_TalonSRX(RobotMap.rightMasterID);
+		rightSlave1 = new WPI_TalonSRX(RobotMap.rightSlave1ID);
+		rightSlave2 = new WPI_TalonSRX(RobotMap.rightSlave2ID);
+		
+		leftMaster.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
+		leftMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+		rightMaster.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 1, 10);
+		rightMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+
+		leftSlave1.follow(leftMaster);
+		leftSlave2.follow(leftMaster);
+		rightSlave1.follow(rightMaster);
+		rightSlave2.follow(rightMaster);	
+	}
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
