04f47329e98db36f7076d90aa73f07dc059cdbcd
frc1987 <broncobots@gmail.com>
2018-03-02 19:57:03 -0600

    jk and this too

---
+++ src/org/usfirst/frc/team1987/robot/commands/auto/GoToRightScaleAndPlace.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team1987.robot.commands.auto;
+
+import org.usfirst.frc.team1987.robot.RobotMap;
+import org.usfirst.frc.team1987.robot.ScaleOwnership;
+import org.usfirst.frc.team1987.robot.commands.SetScaleOwnership;
+import org.usfirst.frc.team1987.robot.commands.claw.EjectCube;
+import org.usfirst.frc.team1987.robot.commands.drive.DrivePath;
+import org.usfirst.frc.team1987.robot.commands.elevator.GoToScaleHeight;
+import org.usfirst.frc.team1987.util.AutoPaths;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class GoToRightScaleAndPlace extends CommandGroup {
+
+	public GoToRightScaleAndPlace() {
+		addSequential(new SetScaleOwnership(ScaleOwnership.NEUTRAL));
+		addSequential(new DrivePath(AutoPaths.toFarScale));
+		addSequential(new WaitCommand(2));
+		addSequential(new GoToScaleHeight());
+		addSequential(new EjectCube());
+	}
+}
