be5a2c8f84b44c476dda28f77973a733a593b25a
LivLittle <olittle0424@gmail.com>
2018-03-09 00:16:50 -0600

    jk and this too
    UNTESTED

--- src/org/usfirst/frc/team1987/robot/commands/auto/RightToLeftScale.java
+++ src/org/usfirst/frc/team1987/robot/commands/auto/RightToLeftScale.java
@@ -20,5 +20,8 @@ public class RightToLeftScale extends CommandGroup {
     	addSequential(new ShiftHigh());
     	addSequential(new DisableCompressor());
     	addSequential(new SetScaleOwnership(ScaleOwnership.DISOWNED));
+    	addSequential(new DrivePath(AutoPaths.rightStartToLeftScale));
+    	addSequential(new GoToScaleHeight());
+    	addSequential(new EjectAndJiggle());
     }
 }
