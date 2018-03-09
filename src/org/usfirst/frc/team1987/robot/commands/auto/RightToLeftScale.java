f865c651fcb1e82153927825d66ecf1245e61f01
LivLittle <olittle0424@gmail.com>
2018-03-09 00:15:01 -0600

    Created RightToLeftScale from RightToLeftScaleLeftSwtich command group
    UNTESTED

---
+++ src/org/usfirst/frc/team1987/robot/commands/auto/RightToLeftScale.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team1987.robot.commands.auto;
+
+import org.usfirst.frc.team1987.robot.ScaleOwnership;
+import org.usfirst.frc.team1987.robot.commands.DisableCompressor;
+import org.usfirst.frc.team1987.robot.commands.SetScaleOwnership;
+import org.usfirst.frc.team1987.robot.commands.claw.EjectAndJiggle;
+import org.usfirst.frc.team1987.robot.commands.drive.DrivePath;
+import org.usfirst.frc.team1987.robot.commands.drive.ShiftHigh;
+import org.usfirst.frc.team1987.robot.commands.elevator.GoToScaleHeight;
+import org.usfirst.frc.team1987.util.AutoPaths;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class RightToLeftScale extends CommandGroup {
+
+    public RightToLeftScale() {
+    	addSequential(new ShiftHigh());
+    	addSequential(new DisableCompressor());
+    	addSequential(new SetScaleOwnership(ScaleOwnership.DISOWNED));
+    }
+}
