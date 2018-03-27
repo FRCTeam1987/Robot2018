5a271a7a9ebac5e0791a19251b48c1d4f47984b0
LivLittle <olittle0424@gmail.com>
2018-03-27 01:13:49 -0500

    Commands in MyAutonomousChooser

--- src/org/usfirst/frc/team1987/robot/MyAutonomousChooser.java
+++ src/org/usfirst/frc/team1987/robot/MyAutonomousChooser.java
@@ -2,6 +2,15 @@ package org.usfirst.frc.team1987.robot;
 
 import org.usfirst.frc.team1987.robot.commands.auto.MiddleToLeftSwitch;
 import org.usfirst.frc.team1987.robot.commands.auto.MiddleToRightSwitch;
+import org.usfirst.frc.team1987.robot.commands.auto.RightToLeftScale;
+import org.usfirst.frc.team1987.robot.commands.auto.RightToLeftScaleExtraSketch;
+import org.usfirst.frc.team1987.robot.commands.auto.RightToLeftScaleLeftSwitch;
+import org.usfirst.frc.team1987.robot.commands.auto.RightToLeftSwitch;
+import org.usfirst.frc.team1987.robot.commands.auto.RightToRightScale;
+import org.usfirst.frc.team1987.robot.commands.auto.RightToRightScale2x;
+import org.usfirst.frc.team1987.robot.commands.auto.RightToRightScale2xWithBackup;
+import org.usfirst.frc.team1987.robot.commands.auto.RightToRightScaleRightSwitch;
+import org.usfirst.frc.team1987.robot.commands.auto.RightToRightSwitch;
 import org.usfirst.frc.team1987.robot.commands.drive.DriveDistance;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -19,32 +28,32 @@ public class MyAutonomousChooser {
 		rightSwitchLeftScaleChooser = new SendableChooser<Command>();
 		rightSwitchRightScaleChooser = new SendableChooser<Command>();
 		
-//		leftSwitchLeftScaleChooser.addDefault("Drive Straight", new DriveDistance(80));
-//		leftSwitchLeftScaleChooser.addObject("Middle to Left Switch", new MiddleToLeftSwitch());
-////		leftSwitchLeftScaleChooser.addDefault("Right to Left Switch", object);
-//		leftSwitchLeftScaleChooser.addObject("Right to Left Scale", object);
-//		leftSwitchLeftScaleChooser.addObject("Right to Left Scale(WIDE)", object);
-//		leftSwitchLeftScaleChooser.addObject("Right to Left Scale Left Switch", object);
-//		
-//		leftSwitchRightScaleChooser.addDefault("Drive Straight", new DriveDistance(80));
-//		leftSwitchRightScaleChooser.addObject("Middle to Left Switch", new MiddleToLeftSwitch());
-////		leftSwitchRightScaleChooser.addObject("Right to Left Switch", object);
-//		leftSwitchRightScaleChooser.addObject("Right to Right Scale", object);
-//		leftSwitchRightScaleChooser.addObject("Right to Right Scale 2X", object);
-//		leftSwitchRightScaleChooser.addObject("Right to Right Scale 2X(WIDE)", object);
-//		
-//		rightSwitchLeftScaleChooser.addDefault("Drive Straight", new DriveDistance(80));
-//		rightSwitchLeftScaleChooser.addObject("Middle to Right Switch", new MiddleToRightSwitch());
-//		rightSwitchLeftScaleChooser.addObject("Right to Right Switch", object);
-//		rightSwitchLeftScaleChooser.addObject("Right to Left Scale", object);
-//
-//		rightSwitchRightScaleChooser.addDefault("Drive Straight", new DriveDistance(80));
-//		rightSwitchRightScaleChooser.addObject("Middle to Right Switch", new MiddleToLeftSwitch());
-//		rightSwitchRightScaleChooser.addObject("Right to Right Switch", object);
-//		rightSwitchRightScaleChooser.addObject("Right to Right Scale", object);
-//		rightSwitchRightScaleChooser.addObject("Right to Right Scale 2X", object);
-//		rightSwitchRightScaleChooser.addObject("Right to Right Scale 2X(WIDE)", object);
-//		rightSwitchRightScaleChooser.addObject("Right to Right Scale Right Switch", object);
+		leftSwitchLeftScaleChooser.addDefault("Drive Straight", new DriveDistance(80));
+		leftSwitchLeftScaleChooser.addObject("Middle to Left Switch", new MiddleToLeftSwitch());
+		leftSwitchLeftScaleChooser.addObject("Right to Left Switch", new RightToLeftSwitch());
+		leftSwitchLeftScaleChooser.addObject("Right to Left Scale", new RightToLeftScale());
+		leftSwitchLeftScaleChooser.addObject("Right to Left Scale(WIDE)", new RightToLeftScaleExtraSketch());
+		leftSwitchLeftScaleChooser.addObject("Right to Left Scale Left Switch", new RightToLeftScaleLeftSwitch());
+		
+		leftSwitchRightScaleChooser.addDefault("Drive Straight", new DriveDistance(80));
+		leftSwitchRightScaleChooser.addObject("Middle to Left Switch", new MiddleToLeftSwitch());
+		leftSwitchRightScaleChooser.addObject("Right to Left Switch", new RightToLeftSwitch());
+		leftSwitchRightScaleChooser.addObject("Right to Right Scale", new RightToRightScale());
+		leftSwitchRightScaleChooser.addObject("Right to Right Scale 2X", new RightToRightScale2x());
+		leftSwitchRightScaleChooser.addObject("Right to Right Scale 2X(WIDE)", new RightToRightScale2xWithBackup());
+		
+		rightSwitchLeftScaleChooser.addDefault("Drive Straight", new DriveDistance(80));
+		rightSwitchLeftScaleChooser.addObject("Middle to Right Switch", new MiddleToRightSwitch());
+		rightSwitchLeftScaleChooser.addObject("Right to Right Switch", new RightToRightSwitch());	//needs testing
+		rightSwitchLeftScaleChooser.addObject("Right to Left Scale", new RightToLeftScale());
+
+		rightSwitchRightScaleChooser.addDefault("Drive Straight", new DriveDistance(80));
+		rightSwitchRightScaleChooser.addObject("Middle to Right Switch", new MiddleToRightSwitch());
+		rightSwitchRightScaleChooser.addObject("Right to Right Switch", new RightToRightSwitch());
+		rightSwitchRightScaleChooser.addObject("Right to Right Scale", new RightToRightScale());
+		rightSwitchRightScaleChooser.addObject("Right to Right Scale 2X", new RightToRightScale2x());
+		rightSwitchRightScaleChooser.addObject("Right to Right Scale 2X(WIDE)", new RightToRightScale2xWithBackup());
+		rightSwitchRightScaleChooser.addObject("Right to Right Scale Right Switch", new RightToRightScaleRightSwitch());
 		
 	}
 }
